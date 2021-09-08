2df9d420bd49aa9b50b813146a1be0394c52e677
Kumar <kumarpndt12@gmail.com>
2021-09-08 17:13:57 +1000

    final solution

--- bookslistingapp.js
+++ bookslistingapp.js
@@ -1,3 +1,29 @@
 import { LightningElement } from 'lwc';
+const BOOK_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
+export default class Bookslistingapp extends LightningElement {
+    query='Harry'
+    books
+    timer
+    connectedCallback(){
+        this.fetchBookData()
+    }
 
-export default class Bookslistingapp extends LightningElement {}
+    fetchBookData(){
+        fetch(BOOK_URL+this.query)
+        .then(response=>response.json())
+        .then(data=>{
+            console.log(data)
+            this.books = data
+        })
+        .catch(error=>console.error(error))
+    }
+//performance efficient technique
+    fetchBooksHandler(event){
+        this.query = event.target.value
+        window.clearInterval(this.timer)
+        this.timer = setTimeout(()=>{
+            this.fetchBookData
+        }, 1000)
+    }
+
+}
